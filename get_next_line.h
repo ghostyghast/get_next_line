267f614a0926195ad051bc7ed15a510826f072c4
amaligno <antoinemalignon@yahoo.com>
2022-11-18 17:41:44 +0800

    First commit, files created

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amaligno <antoinemalignon@yahoo.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/18 17:31:39 by pringles          #+#    #+#             */
+/*   Updated: 2022/11/18 17:36:48 by amaligno         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef	GET_NEXT_LINE_H
+#define	GET_NEXT_LINE_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+#endif
