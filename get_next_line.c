d3c53a3bfba3f4a72c00b06c920f874ab7eb07a2
amaligno <antoine.malignon@yahoo.com>
2022-12-16 16:03:40 +0800

    donno

--- get_next_line.c
+++ get_next_line.c
@@ -6,23 +6,11 @@
 /*   By: amaligno <amaligno@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 17:12:44 by amaligno          #+#    #+#             */
-/*   Updated: 2022/12/13 17:44:34 by amaligno         ###   ########.fr       */
+/*   Updated: 2022/12/16 15:30:11 by amaligno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-char	*newstr(char *stash, char *buf)
-{
-	char		*temp;
-
-	temp = stash;
-	printf("this is temp %s\n", temp);
-	stash = ft_strjoin(temp, buf);
-	if (temp)
-		free(temp);
-	return (stash);
-}
 
 char	*strtrim(char *tocut, char *line)
 {
@@ -46,27 +34,51 @@ char	*strtrim(char *tocut, char *line)
 	return (new);
 }
 
+char	*newstr(char *stash, char *buf, int start)
+{
+	char		*temp;
+
+	if (start == 0)
+	{
+		stash = malloc(1 * sizeof (char));
+		stash[1] = '\0';
+	}
+	// printf("this is stash before strjoin %s]\n", stash);
+	// printf("this is buf %s]\n", buf);
+	temp = ft_strjoin(stash, buf);
+	printf("temp before free %s]\n", temp);
+	free(stash);
+	return (temp);
+}
+
 char	*get_next_line(int fd)
 {
-	char		buf[BUFFER_SIZE + 1];
-	char		*stash;
-	char		*send;
-	int			read_value;
+	char			buf[BUFFER_SIZE + 1];
+	static char		*stash;
+	char			*send;
+	int				read_value;
+	int				start;
 
 	read_value = 1;
+	stash = NULL;
+	if (!stash)
+		start = 0;
 	while (read_value > 0)
 	{
 		read_value = read(fd, buf, BUFFER_SIZE);
 		buf[read_value] = '\0';
-		// printf("[this is buf after read [%s]\n", buf);
-		// printf("this is stash before newstr %s\n", stash);
-		stash = newstr(stash, buf);
-		printf("[this is stash after newstr [%s]\n", stash);
+		printf("this is stash before newstr %s\n", stash);
+		stash = newstr(stash, buf, start);
+		start = 1;
+		printf("[this is start after newstr [%d]\n", start);
+		if (strcheck(stash))
+			break ;
 	}
 	send = malloc(sizeof(char) * (strcheck(stash) + 1));
 	send = memcpy(send, stash, strcheck(stash));
 	printf("this is send %s\n", send);
-	strtrim(send, stash);
+	stash = strtrim(send, stash);
+	printf("this is trim stash [%s]\n", stash);
 	return (send);
 }
 
